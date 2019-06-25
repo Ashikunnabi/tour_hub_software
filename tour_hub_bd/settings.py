75a986d1b9ac262f32b1c89270762be752d8e477
Ashikunnabi <ashikunnabituhin@gmail.com>
2019-06-25 20:58:37 +0600

    Email coppy added

--- tour_hub_bd/settings.py
+++ tour_hub_bd/settings.py
@@ -78,9 +78,6 @@ WSGI_APPLICATION = 'tour_hub_bd.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
