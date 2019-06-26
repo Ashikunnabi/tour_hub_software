6d9df0cf7218d2824cba696b55266c2a11bc9a9c
Ashikunnabi <ashikunnabituhin@gmail.com>
2019-06-26 08:23:27 +0600

    numpy',

--- tour_hub_bd/settings.py
+++ tour_hub_bd/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'numpy',
+    'python-dateutil',
+    'pandas',
     
     'authentication',
     'manager',
